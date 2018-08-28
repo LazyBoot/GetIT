9137cd6dc911dccb94d2578b98851bfe35ef20d1
LazyBoot <github@lazyboot.net>
2018-08-28 14:20:17 +0200

    Oppgave hva gjør koden

---
+++ OppgaveHvaGjorKoden/OppgaveHvaGjorKoden/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OppgaveHvaGjorKoden
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var range = 250;
+            var counts = new int[range];
+            string text = "something";
+            while (!string.IsNullOrWhiteSpace(text))
+            {
+                Array.Clear(counts, 0, counts.Length);
+                var total = 0;
+                text = Console.ReadLine();
+
+                foreach (var character in text?.ToLowerInvariant() ?? string.Empty)
+                {
+                    counts[(int)character]++;
+                    total++;
+                }
+
+                for (var i = 0; i < range; i++)
+                {
+                    if (counts[i] > 0)
+                    {
+                        var character = (char)i;
+                        Console.WriteLine(character + " - " + counts[i].ToString().PadLeft(4) + " - " + Math.Round((float)counts[i] / total * 100).ToString().PadLeft(3) + "%");
+                    }
+                }
+
+            }
+        }
+    }
+}
