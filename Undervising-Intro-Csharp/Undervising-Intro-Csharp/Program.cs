dae424a27f89ce84867d5d928ebf096bacd069d4
LazyBoot <github@lazyboot.net>
2018-08-28 12:40:15 +0200

    Created "Undervisning Intro Csharp"

---
+++ Undervising-Intro-Csharp/Undervising-Intro-Csharp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace UndervisningIntroCS
+{
+    class Program
+    {
+        /* To ting inne i en klasse
+         *      Felt - tilsvarer (nesten) JavaScript globale variabler.
+         *      Metoder - tilsvarer funksjon i JavaScript
+         * 
+         * Pensum i dag:
+         *      C# er et sterkt typet programmeringsspråk
+         *      Intro til Visual Studio
+         *      Debugging
+         *      Grunnskjelettet i en class
+         *      Variabler, tilordning og datatyper
+         *      Valgsetning
+         *      Løkker
+         *      Tabeller
+         *      Metoder (funksjoner)
+         *      Input og output
+         */
+         
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! Antall parametere " + args.Length);
+        }
+    }
+}
