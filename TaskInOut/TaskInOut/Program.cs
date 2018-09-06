47516ac9f381eab1f39fec68857d4a8429f5917e
LazyBoot <github@lazyboot.net>
2018-09-06 15:01:44 +0200

    Missing file

--- TaskInOut/TaskInOut/Program.cs
+++ TaskInOut/TaskInOut/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 
 namespace TaskInOut
 {
@@ -14,46 +13,8 @@ namespace TaskInOut
             //Console.WriteLine(Ciphers.CipherArray(plaintext));
             //Console.WriteLine(Ciphers.DecipherArray(Ciphers.CipherArray(plaintext)));
 
-            var numbers = GenerateNumbers();
-            CountNumbers(numbers);
-        }
-
-        private static int[] GenerateNumbers()
-        {
-            var numbers = new int[40];
-
-            var rnd = new Random();
-            for (var i = 0; i < numbers.Length; i++)
-            {
-                numbers[i] = rnd.Next(9);
-            }
-
-
-            Console.WriteLine(string.Join(" ", numbers));
-            return numbers;
-        }
-
-        private static void CountNumbers(int[] numbers)
-        {
-            var numberCounts = new int[10];
-
-            foreach (var number in numbers)
-            {
-                numberCounts[number]++;
-            }
-
-            var mostNumbers = 0;
-            for (var i = 0; i < numberCounts.Length; i++)
-            {
-                if (i == 0) continue;
-
-                if (numberCounts[i] > numberCounts[mostNumbers])
-                {
-                    mostNumbers = i;
-                }
-            }
-
-            Console.WriteLine($"Number: {mostNumbers} - Counts: {numberCounts[mostNumbers]}");
+            var numbers = NumberCounting.GenerateNumbers();
+            NumberCounting.CountNumbers(numbers);
         }
     }
 
