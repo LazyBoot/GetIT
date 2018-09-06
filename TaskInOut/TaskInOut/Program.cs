c99472b2938ab191ab2006f4f1e961d43ddd63b4
LazyBoot <github@lazyboot.net>
2018-09-06 14:44:51 +0200

    Oppgave 3-7 thinking...

--- TaskInOut/TaskInOut/Program.cs
+++ TaskInOut/TaskInOut/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace TaskInOut
 {
@@ -9,9 +10,39 @@ namespace TaskInOut
             // var intArray = new int[6];
             //int[] intArray = { 1, 2, 3, 4, 5,94,1 };
             //Console.WriteLine(IsArraySorted.isArraySorted(intArray));
-            string plaintext = "Ibsens Ripsbaerbusker og andre buksvekster.";
-            Console.WriteLine(Ciphers.CipherArray(plaintext));
-            Console.WriteLine(Ciphers.DecipherArray(Ciphers.CipherArray(plaintext)));
+            //string plaintext = "Ibsens Ripsbaerbusker og andre buksvekster.";
+            //Console.WriteLine(Ciphers.CipherArray(plaintext));
+            //Console.WriteLine(Ciphers.DecipherArray(Ciphers.CipherArray(plaintext)));
+
+            var numbers = new int[40];
+
+            var rnd = new Random();
+            for (var i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rnd.Next(9);
+            }
+
+
+            Console.WriteLine(string.Join(" ", numbers));
+            var numberCounts = new int[10];
+
+            foreach (var number in numbers)
+            {
+                numberCounts[number]++;
+            }
+
+            var mostNumbers = 0;
+            for (var i = 0; i < numberCounts.Length; i++)
+            {
+                if (i == 0) continue;
+
+                if (numberCounts[i] > numberCounts[mostNumbers])
+                {
+                    mostNumbers = i;
+                }
+            }
+
+            Console.WriteLine($"Number: {mostNumbers} - Counts: {numberCounts[mostNumbers]}");
         }
     }
 
