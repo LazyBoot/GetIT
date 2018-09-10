4330ddc0599a38fd3f1356093cc7f4b1a0bf6903
LazyBoot <github@lazyboot.net>
2018-09-10 14:38:15 +0200

    undervisning 3

--- UndervisningObjekter/Undervisning3/Program.cs
+++ UndervisningObjekter/Undervisning3/Program.cs
@@ -12,14 +12,41 @@ namespace Undervisning3
         {
             //Demo1();
             //Demo2();
+            //DemoString();
+            DemoSideEffects();
+        }
 
-            DemoString();
+        private static void DemoSideEffects()
+        {
+            var box = new Box { Width = 20, Height = 30 };
+            int i = 7;
+
+            Console.WriteLine("Før: i=" + i);
+            Console.WriteLine("Før: box.Height=" + box.Height);
+
+            DoSomething(box, i);
+
+            Console.WriteLine("Etter: i=" + i);
+            Console.WriteLine("Etter: box.Height=" + box.Height);
+        }
+
+        private static void DoSomething(Box box, int i)
+        {
+            i += 10;
+            box.Height += 10;
         }
 
         private static void DemoString()
         {
+            // String Immutable C#
+            // String Interning C#
+
             string a = "A";
             string b = "A";
+
+            if (a == b) Console.WriteLine("A og B er like");
+            else Console.WriteLine("A og B er ikke like");
+
         }
 
         private static void Demo2()
@@ -50,6 +77,21 @@ namespace Undervisning3
 
             var c = boxA;
             c.Height = 99;
+        }
+        private static void Demo3()
+        {
+            /* Reference types
+             *  - objekter
+             *  vs
+             * Value types
+             *  - int, float, bool, double, char
+             */
+
+
+            var boxA = new Box { Width = 20, Height = 30 };
+            var boxB = new Box { Width = 5, Height = 7 };
+
+            boxA = boxB;
         }
     }
 }
