056ae4f1daf06e9fcd5a6dd4c8c524e4c78391ab
LazyBoot <github@lazyboot.net>
2018-09-11 13:43:05 +0200

    Made method signature more logical

--- TreRadOO/TreRadOO/Game.cs
+++ TreRadOO/TreRadOO/Game.cs
@@ -6,18 +6,18 @@
 
         public static bool CheckWin(BoardModel bm)
         {
-            return CheckCells(0, 1, 2, bm)
-                   || CheckCells(3, 4, 5, bm)
-                   || CheckCells(6, 7, 8, bm)
-                   || CheckCells(0, 3, 6, bm)
-                   || CheckCells(1, 4, 7, bm)
-                   || CheckCells(2, 5, 8, bm)
-                   || CheckCells(0, 4, 8, bm)
-                   || CheckCells(2, 4, 6, bm);
+            return CheckCells(bm, 0, 1, 2)
+                   || CheckCells(bm, 3, 4, 5)
+                   || CheckCells(bm, 6, 7, 8)
+                   || CheckCells(bm, 0, 3, 6)
+                   || CheckCells(bm, 1, 4, 7)
+                   || CheckCells(bm, 2, 5, 8)
+                   || CheckCells(bm, 0, 4, 8)
+                   || CheckCells(bm, 2, 4, 6);
 
         }
 
-        private static bool CheckCells(int cellIndex0, int cellIndex1, int cellIndex2, BoardModel bm)
+        private static bool CheckCells(BoardModel bm, int cellIndex0, int cellIndex1, int cellIndex2)
         {
             var cell0 = bm.Cells[cellIndex0];
             var cell1 = bm.Cells[cellIndex1];
