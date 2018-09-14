3a36e2cb8631e8307e0b755c8cb999c386d51022
LazyBoot <github@lazyboot.net>
2018-09-14 12:54:39 +0200

    Code cleanup

--- TilfeldigFirkant/TilfeldigFirkantTest/BoxTest.cs
+++ TilfeldigFirkant/TilfeldigFirkantTest/BoxTest.cs
@@ -10,12 +10,24 @@ namespace TilfeldigFirkantTest
         [TestMethod]
         public void TestBox()
         {
-            var box = new Box(2,3,4,5);
+            var box = new Box(2, 3, 4, 5);
 
-            Assert.AreEqual(2,box.X);
-            Assert.AreEqual(3,box.Y);
-            Assert.AreEqual(4,box.Width);
-            Assert.AreEqual(5,box.Height);
+            Assert.AreEqual(2, box.X);
+            Assert.AreEqual(3, box.Y);
+            Assert.AreEqual(4, box.Width);
+            Assert.AreEqual(5, box.Height);
+        }
+
+        [TestMethod]
+        public void TestRandomBox()
+        {
+            var random = new Random();
+            var box = new Box(random, 9, 9);
+
+            Assert.IsNotNull(box.X);
+            Assert.IsNotNull(box.Y);
+            Assert.AreNotEqual(0, box.Width);
+            Assert.AreNotEqual(0, box.Height);
         }
 
         [TestMethod]
@@ -26,8 +38,8 @@ namespace TilfeldigFirkantTest
             var topY = box.GetTopRowY();
             var bottomY = box.GetBottomRowY();
 
-            Assert.AreEqual(3,topY);
-            Assert.AreEqual(8,bottomY);
+            Assert.AreEqual(3, topY);
+            Assert.AreEqual(8, bottomY);
         }
 
     }
