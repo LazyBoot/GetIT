304b06099660e3f7226a7bbd51c7a1b2845b333e
LazyBoot <github@lazyboot.net>
2018-09-18 23:32:50 +0200

    blabla

--- StartList/StartList/Program.cs
+++ StartList/StartList/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualBasic.FileIO;
 
 namespace StartList
 {
@@ -10,6 +12,54 @@ namespace StartList
     {
         static void Main(string[] args)
         {
+            var file = "startlist.csv";
+
+            var registrations = UsingParser(file);
+        }
+
+        private static List<Registration> UsingParser(string file)
+        {
+            var registrations = new List<Registration>();
+
+            using (var parser = new TextFieldParser(file))
+            {
+                parser.SetDelimiters(",");
+                if (!parser.EndOfData)
+                    parser.ReadFields();
+
+                while (!parser.EndOfData)
+                {
+                    var fields = parser.ReadFields();
+                    registrations.Add(new Registration(fields[0], fields[1], fields[2], fields[3], fields[4], fields[5]));
+                }
+            }
+
+            return registrations;
+        }
+
+        private static List<Registration> UsingStreamReader(string file)
+        {
+            var registrations = new List<Registration>();
+
+            using (var reader = new StreamReader(file))
+            {
+                reader.ReadLine();
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var fields = line.Split(',');
+                    var startNumber = fields[0].Trim('"');
+                    var name = fields[1].Trim('"');
+                    var club = fields[2].Trim('"');
+                    var nationality = fields[3].Trim('"');
+                    var @group = fields[4].Trim('"');
+                    var @class = fields[5].Trim('"');
+                    registrations.Add(new Registration(startNumber, name, club, nationality, @group, @class));
+                }
+            }
+
+            return registrations;
+
         }
     }
 }
---
+++ StartList/StartList/Registration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartList
+{
+    public class Registration
+    {
+        public string StartNumber;
+        public string Name;
+        public string Club;
+        public string Nationality;
+        public string Group;
+        public string Class;
+
+        public Registration(string startNumber, string name, string club, string nationality, string group, string @class)
+        {
+            StartNumber = startNumber;
+            Name = name;
+            Club = club;
+            Nationality = nationality;
+            Group = group;
+            Class = @class;
+        }
+    }
+}
