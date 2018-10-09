a11c3267c5e92c9aa82be8f03f04164a2b633d06
LazyBoot <github@lazyboot.net>
2018-10-09 09:00:45 +0200

    Oppgave interface

---
+++ OppgaveInterface/OppgaveInterface/Program.cs
@@ -0,0 +1,32 @@
+﻿using PomodoroEngine;
+using System;
+
+namespace OppgaveInterface
+{
+    class Program : INotifyObject
+    {
+        static void Main(string[] args)
+        {
+            var program = new Program();
+            Console.ReadLine();
+        }
+
+        public Program()
+        {
+            var pomodoro = new Pomodoro(this, 1);
+            pomodoro.Start();
+        }
+
+        public void Tick(int minutes, int seconds)
+        {
+            Console.Clear();
+
+            if (minutes == 0 && seconds == 0)
+            {
+                Console.WriteLine("Ta pause");
+                Environment.Exit(0);
+            }
+            Console.WriteLine($"{minutes}:{seconds}");
+        }
+    }
+}
