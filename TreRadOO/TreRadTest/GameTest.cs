5618fdca2dc80fab9e56221f06c39ff7cbe0b142
LazyBoot <github@lazyboot.net>
2018-10-23 14:27:29 +0200

    Separate winner test into mulitple tests for each combinaiton

--- TreRadOO/TreRadTest/GameTest.cs
+++ TreRadOO/TreRadTest/GameTest.cs
@@ -11,32 +11,82 @@ namespace TreRadTest
         [TestMethod]
         public void TestWinner()
         {
-            var bm = new BoardModel();
-
-            TestCells(bm, 0, 1, 2);
-            TestCells(bm, 3, 4, 5);
-            TestCells(bm, 6, 7, 8);
-            TestCells(bm, 0, 3, 6);
-            TestCells(bm, 1, 4, 7);
-            TestCells(bm, 2, 5, 8);
-            TestCells(bm, 0, 4, 8);
-            TestCells(bm, 2, 4, 6);
-
-            
+            var bm = GetTestBoard(0, 1, 2);
+            Assert.IsTrue(Game.CheckWin(bm));
+            Assert.AreEqual("Du", Game.Winner);
         }
 
-        private void TestCells(BoardModel bm, int cell1, int cell2, int cell3)
+        [TestMethod]
+        public void TestWinner2()
         {
+            var bm = GetTestBoard(3, 4, 5);
+            Assert.IsTrue(Game.CheckWin(bm));
+            Assert.AreEqual("Du", Game.Winner);
+        }
+
+        [TestMethod]
+        public void TestWinner3()
+        {
+            var bm = GetTestBoard(6, 7, 8);
+            Assert.IsTrue(Game.CheckWin(bm));
+            Assert.AreEqual("Du", Game.Winner);
+        }
+
+        [TestMethod]
+        public void TestWinner4()
+        {
+            var bm = GetTestBoard(0, 3, 6);
+            Assert.IsTrue(Game.CheckWin(bm));
+            Assert.AreEqual("Du", Game.Winner);
+        }
+
+        [TestMethod]
+        public void TestWinner5()
+        {
+            var bm = GetTestBoard(1, 4, 7);
+            Assert.IsTrue(Game.CheckWin(bm));
+            Assert.AreEqual("Du", Game.Winner);
+        }
+
+        [TestMethod]
+        public void TestWinner6()
+        {
+            var bm = GetTestBoard(2, 5, 8);
+            Assert.IsTrue(Game.CheckWin(bm));
+            Assert.AreEqual("Du", Game.Winner);
+        }
+
+        [TestMethod]
+        public void TestWinner7()
+        {
+            var bm = GetTestBoard(0, 4, 8);
+            Assert.IsTrue(Game.CheckWin(bm));
+            Assert.AreEqual("Du", Game.Winner);
+        }
+
+        [TestMethod]
+        public void TestWinner8()
+        {
+            var bm = GetTestBoard(2, 4, 6);
+            Assert.IsTrue(Game.CheckWin(bm));
+            Assert.AreEqual("Du", Game.Winner);
+        }
+
+        [TestMethod]
+        public void TestNoWinner()
+        {
+            var bm = new BoardModel();
+            Assert.IsFalse(Game.CheckWin(bm));
+            Assert.AreEqual(null, Game.Winner);
+        }
+
+        private static BoardModel GetTestBoard(int cell1, int cell2, int cell3)
+        {
+            var bm = new BoardModel();
             bm.Cells[cell1].SetSymbol(CellOwner.Player1);
             bm.Cells[cell2].SetSymbol(CellOwner.Player1);
             bm.Cells[cell3].SetSymbol(CellOwner.Player1);
-
-            Game.CheckWin(bm);
-            Assert.AreEqual("Du", Game.Winner);
-
-            bm.Cells[cell1].SetSymbol(CellOwner.None);
-            bm.Cells[cell2].SetSymbol(CellOwner.None);
-            bm.Cells[cell3].SetSymbol(CellOwner.None);
+            return bm;
         }
 
     }
