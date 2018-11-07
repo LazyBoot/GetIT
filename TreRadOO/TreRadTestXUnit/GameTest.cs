0f94a57ad12132ff172d4d47b11597af17564fd1
LazyBoot <github@lazyboot.net>
2018-11-07 14:13:13 +0100

    Fix test data

--- TreRadOO/TreRadTestXUnit/GameTest.cs
+++ TreRadOO/TreRadTestXUnit/GameTest.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using TreRadOO;
-using TreRadTestXUnit.TestData;
+﻿using TreRadOO;
 using Xunit;
 
 namespace TreRadTestXUnit
--- TreRadOO/TreRadTestXUnit/GameTestData.cs
+++ TreRadOO/TreRadTestXUnit/GameTestData.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Text;
+using TreRadOO;
 
 namespace TreRadTestXUnit
 {
@@ -9,14 +10,32 @@ namespace TreRadTestXUnit
 	{
 		private readonly List<object[]> _data = new List<object[]>
 		{
-			new object[] {0, 1, 2},
-			new object[] {3, 4, 5},
-			new object[] {6, 7, 8},
-			new object[] {0, 3, 6},
-			new object[] {1, 4, 7},
-			new object[] {2, 5, 8},
-			new object[] {0, 4, 8},
-			new object[] {2, 4, 6},
+			new object[] {0, 1, 2, CellOwner.Player1},
+			new object[] {3, 4, 5, CellOwner.Player1},
+			new object[] {6, 7, 8, CellOwner.Player1},
+			new object[] {0, 3, 6, CellOwner.Player1},
+			new object[] {1, 4, 7, CellOwner.Player1},
+			new object[] {2, 5, 8, CellOwner.Player1},
+			new object[] {0, 4, 8, CellOwner.Player1},
+			new object[] {2, 4, 6, CellOwner.Player1},
+
+			new object[] {0, 1, 2, CellOwner.Player2},
+			new object[] {3, 4, 5, CellOwner.Player2},
+			new object[] {6, 7, 8, CellOwner.Player2},
+			new object[] {0, 3, 6, CellOwner.Player2},
+			new object[] {1, 4, 7, CellOwner.Player2},
+			new object[] {2, 5, 8, CellOwner.Player2},
+			new object[] {0, 4, 8, CellOwner.Player2},
+			new object[] {2, 4, 6, CellOwner.Player2},
+
+			new object[] {0, 1, 2, CellOwner.None},
+			new object[] {3, 4, 5, CellOwner.None},
+			new object[] {6, 7, 8, CellOwner.None},
+			new object[] {0, 3, 6, CellOwner.None},
+			new object[] {1, 4, 7, CellOwner.None},
+			new object[] {2, 5, 8, CellOwner.None},
+			new object[] {0, 4, 8, CellOwner.None},
+			new object[] {2, 4, 6, CellOwner.None},
 		};
 
 		public IEnumerator<object[]> GetEnumerator() => _data.GetEnumerator();
