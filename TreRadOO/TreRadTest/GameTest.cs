889fad1ba861e868ab3c8b5da3ad79402f311402
LazyBoot <github@lazyboot.net>
2018-11-07 13:34:25 +0100

    .

--- TreRadOO/TreRadTest/GameTest.cs
+++ TreRadOO/TreRadTest/GameTest.cs
@@ -1,7 +1,5 @@
 ﻿using NUnit.Framework;
-using System.Collections.Generic;
 using TreRadOO;
-using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
 
 namespace TreRadTest
 {
@@ -36,22 +34,4 @@ namespace TreRadTest
         }
 
     }
-
-	public class GameTestData
-	{
-		public static IEnumerable<TestCaseData> TestWinnerData
-		{
-			get
-			{
-				yield return new TestCaseData(0,1,2);
-				yield return new TestCaseData(3,4,5);
-				yield return new TestCaseData(6,7,8);
-				yield return new TestCaseData(0,3,6);
-				yield return new TestCaseData(1,4,7);
-				yield return new TestCaseData(2,5,8);
-				yield return new TestCaseData(0,4,8);
-				yield return new TestCaseData(2,4,6);
-			}
-		}
-	}
 }
